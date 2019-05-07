84fbc7a106c1f8b80f4b5c8e41e1708a2c375425
vinubhat092 <49424792+vinubhat092@users.noreply.github.com>
2019-05-07 11:36:28 +0530

    Create vik.c

---
+++ vik.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int getinput()
+int add(int, int)
+void output(int)
+int main()
+{
+   int x;
+   printf("read value\n");
+   scanf("%d",&x);
+   return x;
+}
+   int add(intx ,inty);
+{  
+   return(x+y);
+}  
+   void output(int p)
+{
+   printf(sum is &d,p};
+   return;
+}
+ int main()
+{
+   getinput(int a);
+   getinput(int b);
+   sum=(inta+intb);
+   output(sum);
+   return;
+} 
