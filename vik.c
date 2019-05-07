de7337b0e3071dc9a726d9d7642723823713e67f
vinubhat092 <49424792+vinubhat092@users.noreply.github.com>
2019-05-07 12:02:51 +0530

    Update vik.c

--- vik.c
+++ vik.c
@@ -1,28 +1,30 @@
+
 #include<stdio.h>
-int getinput()
-int add(int, int)
-void output(int)
+int getinput();
+int add(int, int);
+void output(int);
 int main()
+{
+   int a,b,sum;
+   a=getinput();
+   b=getinput();
+   sum=add(a,b);
+   output(sum);
+   return 0;
+}
+int getinput()
 {
    int x;
    printf("read value\n");
    scanf("%d",&x);
    return x;
 }
-   int add(intx ,inty);
-{  
-   return(x+y);
-}  
-   void output(int p)
+int add(int x,int y);
 {
-   printf(sum is &d,p};
+   return(x+y);
+}
+void output(int p)
+{
+   printf("the sum is %d",p);
    return;
 }
- int main()
-{
-   getinput(int a);
-   getinput(int b);
-   sum=(inta+intb);
-   output(sum);
-   return;
-} 
