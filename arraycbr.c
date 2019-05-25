348c8240aa9911770b0493d4bf14bf35e8c2ec1c
vinubhat092 <49424792+vinubhat092@users.noreply.github.com>
2019-05-24 21:00:48 -0700

    Update arraycbr.c

--- arraycbr.c
+++ arraycbr.c
@@ -1,9 +1,35 @@
 #include<stdio.h>
 void input(int *n)
 {
-    scanf("%d",&n);
+    scanf("%d",n);
 }
-void inputnumbers(int n,int a[n]);
+void inputnumbers(int n,int a[n])
 {
+    for(i=0;i<n;i++)
+    {
+        printf("print the array elements");
+        scanf("%d",&a[i]);
+    }
+    return;
+}
+void compute(int n,int [n],int *sum)
+{
+    sum=0;
+    s+=a[n];
+    return;
+}
+void output(int sum)
+{
+    printf("print the sum of array numbers",sum)
+    return;
+}
+void main()
+{
+    input(&n);
+    compute(n,[n],&sum);
+    s+=a[n];
     
+        
+    
+   
     
